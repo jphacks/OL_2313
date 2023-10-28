0c44f09edfef711e547183e904eb83bdb1e5c806
mommom111 <20l1165@andrew.ac.jp>
2023-10-28 13:25:25 +0900

    update: 画像をクリックすると選択した要素が表示される。

--- react/src/components/Archive.tsx
+++ react/src/components/Archive.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
+import { getDocs, collection, getDoc, updateDoc, doc, Timestamp, Query, query, orderBy } from "firebase/firestore";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import DetailOfArchive from "./DetailOfArchive";
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
+interface Post {
+  id?: string;
+  title: string;
+  imageUrl: string;
+  tags: string[];
+  likes: number;
+  uploadedAt: Timestamp;
+}
 
-function Archive() {
+const Archive: React.FC = () =>{
+
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const fetchPosts = async () => {
+    const postQuery: Query = query(collection(db, "your-collection"), orderBy("uploadedAt", "desc"));
+    const postCollection = await getDocs(postQuery);
+    setPosts(postCollection.docs.map((doc) => ({ ...doc.data(), id: doc.id } as Post)));
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const handleClick = (postId: string) => {
+    console.log(postId);
+  }
+
   return (
-    <div>
-      <h1>画像ギャラリー</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        {posts.map((post, index) => (
+          <Link to={`/${post.id}`}>
+            <img onClick={() => post.id && handleClick(post.id)} src={post.imageUrl} alt={post.title} className="post-image" />
+          </Link>
+        ))}
+
+        <Routes>
+          <Route path="/:postId" element={<DetailOfArchive />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default Archive;
-
-
-
-
-
