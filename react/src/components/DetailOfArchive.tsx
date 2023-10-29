49650faa60548612fadb0e509779c26fe5e93340
mommom111 <20l1165@andrew.ac.jp>
2023-10-29 10:08:48 +0900

    Update: archiveとDetailOfArchiveの装飾

--- react/src/components/DetailOfArchive.tsx
+++ react/src/components/DetailOfArchive.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { db } from "../firebase";
 import { getDocs, collection, getDoc, updateDoc, doc, Timestamp, Query, query, orderBy } from "firebase/firestore";
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 interface Post {
   id?: string;
@@ -23,35 +24,36 @@ const DetailOfArchive: React.FC = () => {
 
   useEffect(() => {
     if (postId) {
-      // Firebaseから指定のpostIdのデータを取得
       const postRef = doc(db, "your-collection", postId);
       
       getDoc(postRef)
         .then((docSnapshot) => {
           if (docSnapshot.exists()) {
-            // データが存在する場合、データを取得
             setPost({ ...docSnapshot.data(), id: postId } as Post);
           } else {
-            // データが存在しない場合、エラー処理を行うか、メッセージを表示できます
             console.log("データが見つかりません");
           }
         })
         .catch((error) => {
-          // エラーハンドリングを行うことも重要です
           console.error("データの取得中にエラーが発生しました", error);
         });
     }
   }, [postId]);
 
   return (
-    <div>
+    <div className='archive-detail-content'>
       {post ? (
         <div>
-          <h1>{post.title}</h1>
-          <img src={post.imageUrl} alt={post.title} />
-          <p>Tags: {post.tags.join(", ")}</p>
-          <p>Likes: {post.likes}</p>
-          {/* その他の詳細情報を表示 */}
+          <img src={post.imageUrl} alt={post.title} className="post-image" />
+          <p className='detail-text'>
+            <h1>Player_11</h1>
+            <p className='archive-detail-flex'>
+              <p><FavoriteIcon style={{marginRight: 8, color: "red"}}/></p>
+              <p style={{color: "red"}}>{post.likes}</p>
+            </p>
+            <p className='detail-tag'>#{post.tags.join(", ")}</p>
+            <p className='detail-timestamp'>{post.uploadedAt ? new Date(post.uploadedAt.seconds * 1000).toLocaleString() : "No Timestamp"}</p>
+          </p>
         </div>
       ) : (
         <p>データを取得中...</p>
